3b9130a31c6354cca1f924720ca192e52ae24cf3
Matheus <matheus@respondeai.com.br>
2021-09-14 15:51:17 -0300

    preparando o review

--- src/components/Loader.js
+++
@@ -1,7 +0,0 @@
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import Loader from "react-loader-spinner";
-
-function StyledLoader() {
-	return <Loader type="ThreeDots" color="white" height={50} width={50} />;
-}
-export default StyledLoader;
